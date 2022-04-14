eb9036bd7961be50f613154bccc1423e25f0aaa7
Rommanirs Mer <rommanirs.mer@outlook.fr>
2022-04-14 17:07:35 +0200

    add a* and MoveTo stateMachine

--- AR-Garbage-Collector/Assets/Scripts/MoveTo.cs
+++ AR-Garbage-Collector/Assets/Scripts/MoveTo.cs
@@ -1,67 +1,86 @@
+using Pathfinding;
 using UnityEngine;
-using UnityEngine.AI;
+
+public enum CarState
+{
+    Idl,CheckMission,LaunchTour,InTour
+}
 
 public class MoveTo : MonoBehaviour
 {
-    private GameObject _goal;
     [SerializeField] public int minimumTrashCan;
     [SerializeField] public bool isAlwaysOperating;
+    
     public TrashCanManager trashCanManager;
-    private NavMeshAgent _agent;
-    private bool collectMode;
-    // Start is called before the first frame update
+    public AIDestinationSetter destination;
+    private GameObject goal;
+    private CarState currentState;
+
     void Start()
     {
         trashCanManager = FindObjectOfType<TrashCanManager>();
-        _agent = GetComponent<NavMeshAgent>();
-        collectMode = false;
+        destination = FindObjectOfType<AIDestinationSetter>();
     }
     void Update()
     {
-        UpdateOperationMode();
-        CheckCurrentMission();
+        switch (currentState)
+        {
+            case CarState.Idl:
+                UpdateOperationMode();
+                break;
+            case CarState.CheckMission:
+                CheckMission();
+                break;
+            case CarState.LaunchTour: 
+                SetNewMission();
+                break;
+            case CarState.InTour:
+                CheckMission();
+                break;
+        }
     }
     public void UpdateOperationMode()
     {
         if (trashCanManager.GetFilledTrashCan() >= minimumTrashCan | isAlwaysOperating)
         {
             Debug.Log("Trash can collection started");
-            collectMode = true;
+            CheckMission();
         }
         else
         {
             Debug.Log("Trash can collection halted");
-            collectMode = false;
+            destination.target = null;
         }
     }
-    public void CheckCurrentMission()
+    public void CheckMission()
     {
-        if (_goal == null )
+        if (goal == null )
         {
             if (trashCanManager.GetFilledTrashCan() > 0)
             {
-                GetNewMission();
+                goal = trashCanManager.GetTask();
+                Debug.Log("New mission assign target is now: " + goal.gameObject.name);
+                currentState = CarState.LaunchTour;
             }
         }
-
-        if (_goal != null)
+        if (goal != null)
         {
-            if (!_goal.GetComponent<TrashCan>()._isFull)
+            if (!goal.GetComponent<TrashCan>()._isFull)
             {
                 if (trashCanManager.GetFilledTrashCan() > 0)
                 {
-                    GetNewMission();
-                    Debug.Log("New mission assign target is now: " + _goal.gameObject.name);
+                    goal = trashCanManager.GetTask();
+                    Debug.Log("New mission assign target is now: " + goal.gameObject.name);
+                    currentState = CarState.LaunchTour;
                 }
             }
         }
+        
     }
-    public void GetNewMission()
+    public void SetNewMission()
     {
-        if (collectMode)
-        {
-            _goal = trashCanManager.GetTask();
-            _agent.destination = _goal.transform.position;
-        }
+        Debug.Log("Current mission is " + goal.transform + " position");
+        destination.target = goal.transform;
+        currentState = CarState.InTour;
     }
 }
