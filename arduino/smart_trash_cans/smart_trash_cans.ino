1dfcab8cc1c3c3a4853d944cd4e7eb660fc7356f
Dschmbck.Thms <thomas.deschombeck@epitech.eu>
2022-04-15 17:15:17 +0200

    integrate Photoresistor

--- arduino/smart_trash_cans/smart_trash_cans.ino
+++ arduino/smart_trash_cans/smart_trash_cans.ino
@@ -1,15 +1,19 @@
 #include "EspMQTTClient.h"
 
+// PHOTO RESISTOR : 3v3 = entrée PR | GND = 10kO | A0 = Sortie avec resistance
+const int ldrPin = A0;
+
+// IMU : D1 = SLC | D2 = SDA | 3V3 = VCC | GND = GND
+
+
 const String topic = "STC/sensor";
-// const char* ssid = "Livebox-D280";
-// const char* password = "22061995";
 const char* ssid = "AP_Thomas";
 const char* password = "122345678";
 
 const String serialNumber = "16543465453";
-const int sensorType = 1; // 1 = Tilt | 2 = Photo | 3 = IMU
+const int sensorType = 2; // 1 = Tilt | 2 = Photo | 3 = IMU
 
-bool state = false;
+bool trashCanIsFull = false;
 
 EspMQTTClient client(
   ssid,
@@ -26,58 +30,93 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println("====Projet DEV - Setup====");
+
+  //publishMeasure();
+  
+  initSensor();
 }
  
 void loop() 
 {
-  //publishMeasure();
-  
   Serial.print(".");
-  
   client.loop();
 
-  if(stateChanged()){
-    state = !state;
-    publishMeasure();
-  }
+  checkSensor();
 
-  delay(2000); 
+  delay(1000); 
 }
+
+void checkSensor(){
+  switch(sensorType){
+    case 1:
+      // TODO      
+      break;
+    case 2:
+      checkPhotoResistor();
+      break;      
+    case 3:
+      // TODO      
+      break;
+  }
+}
+
+void initSensor(){
+    switch(sensorType){
+    case 1:
+      initTilt();
+      break;
+    case 2:
+      initPhotoResistor();
+      break;      
+    case 3:
+      initIMU();
+      break;
+  }
+}
+
+void initTilt(){
+  // TODO  
+}
+
+void initIMU(){
+  // TODO  
+}
+
+// ===== PHOTO RESISTOR =====
+
+void initPhotoResistor(){
+  pinMode(ldrPin, INPUT);  
+}
+
+void checkPhotoResistor(){
+
+  if(photoResistorIsOpen()){
+    Serial.println("La poubelle est ouverte");
+
+    while(photoResistorIsOpen()){
+      delay(500);
+    }
+
+    Serial.println("La poubelle est refermée");
+    trashCanIsFull = !trashCanIsFull;
+    publishMeasure();    
+  }
+  
+  Serial.print("Etat de la poubelle : ");
+  Serial.println(trashCanIsFull);
+}
+
+bool photoResistorIsOpen(){
+  return analogRead(ldrPin) >= 500 ? true : false; 
+}
+
+// ===== MQTT =====
 
 void publishMeasure() {
   if(client.isConnected()){
-    String strState = state ? "1" : "0";    
+    String strState = trashCanIsFull ? "1" : "0";    
     client.publish(topic, "{\"value\":" + strState + ",\"sn\":\"" + serialNumber + "\"}");
   }
-}
-
-bool stateChanged(){
-  switch(sensorType){
-    case 1:
-      return readTiltSensor() != state;
-      break;
-    case 2:
-      return readPhotoSensor() != state;
-      break;      
-    case 3:
-      return readIMU() != state;
-      break;
-  }
-}
-
-bool readTiltSensor(){
-  //TODO
-  return true;
-}
-
-bool readPhotoSensor(){
-  //TODO
-  return true;
-}
-
-bool readIMU(){
-  //TODO
-  return true;
 }
 
 void onConnectionEstablished(){
